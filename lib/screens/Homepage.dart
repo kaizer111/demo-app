f9e27c3090be7c3ee2b3e8ed22e0f7c3eb18282c
SUMIT KUMAR MAHATO <75285482+Kalki2307@users.noreply.github.com>
2022-10-14 18:07:25 +0530

    adding drawer

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -9,6 +9,43 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text('Tshirt lena hai'),
       ),
+     drawer: Drawer(  
+        child: ListView(  
+          // Important: Remove any padding from the ListView.  
+          padding: EdgeInsets.zero,  
+          children: <Widget>[  
+            UserAccountsDrawerHeader(  
+              accountName: Text("Abhishek Mishra"),  
+              accountEmail: Text("abhishekm977@gmail.com"),  
+              currentAccountPicture: CircleAvatar(  
+                backgroundColor: Colors.orange,  
+                child: Text(  
+                  "A",  
+                  style: TextStyle(fontSize: 40.0),  
+                ),  
+              ),  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.home), title: Text("Home"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.settings), title: Text("Settings"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+            ListTile(  
+              leading: Icon(Icons.contacts), title: Text("Contact Us"),  
+              onTap: () {  
+                Navigator.pop(context);  
+              },  
+            ),  
+          ],  
+        ),  
+      ),  
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
