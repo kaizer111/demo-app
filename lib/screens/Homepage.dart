e55b2170f8d43f15f43fff1c2e0af132f3bacfde
Arya Dhrangdhria <adhrangdhria8@gmail.com>
2022-10-14 18:39:15 +0530

    Added Drawers and Updated pubspec.yaml dart dependency. And some linting.

--- lib/screens/Homepage.dart
+++ lib/screens/Homepage.dart
@@ -1,18 +1,47 @@
 import 'package:flutter/material.dart';
 
 class Homepage extends StatelessWidget {
- 
+  const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: Drawer(
+          child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text("Drawer"),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 3'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('Item 4'),
+            onTap: () {},
+          ),
+        ],
+      )),
       appBar: AppBar(
-        title: Text('Tshirt lena hai'),
+        title: const Text('Tshirt lena hai'),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.green,
-        onPressed: () {
-        }
-       ),);}}
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.add),
+          onPressed: () {}),
+    );
+  }
+}
